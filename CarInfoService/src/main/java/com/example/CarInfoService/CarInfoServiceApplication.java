0d4edcb8650e8d9ed840f010f364fd6247fdb9d1
N3nadM <nenad.marin96@gmail.com>
2020-05-21 14:57:50 +0200

    inicijalni projekti za mikroservise

--- CarInfoService/src/main/java/com/example/CarInfoService/CarInfoServiceApplication.java
+++ CarInfoService/src/main/java/com/example/CarInfoService/CarInfoServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.CarInfoService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CarInfoServiceApplication {
 
 	public static void main(String[] args) {
--- RequestService/src/main/java/com/example/RequestService/RequestServiceApplication.java
+++ RequestService/src/main/java/com/example/RequestService/RequestServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.RequestService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RequestServiceApplication {
 
 	public static void main(String[] args) {
