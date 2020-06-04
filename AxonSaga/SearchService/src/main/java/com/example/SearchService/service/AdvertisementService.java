af3ed19ef99763dc2e52b7ec94f54286764e83c7
unknown <kristina.radojevic28@gmail.com>
2020-06-04 20:48:16 +0200

    ispravljanje datuma

--- AxonSaga/SearchService/src/main/java/com/example/SearchService/service/AdvertisementService.java
+++ AxonSaga/SearchService/src/main/java/com/example/SearchService/service/AdvertisementService.java
@@ -6,6 +6,8 @@ import com.example.SearchService.repository.AdvertisementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -16,6 +18,8 @@ public class AdvertisementService {
 
     public Advertisement addAdvertisement(AdvertisementDTO dto){
         //za sada se za obe cene stavlja ona koju je user definisao jer jos nemamo cenovnik
+        dto.setFreeFrom(getMidnightStartDate(dto.getFreeFrom()).getTime());
+        dto.setFreeTo(getMidnightEndDate(dto.getFreeTo()).getTime());
         Advertisement advertisement = new Advertisement(dto);
         return advertisementRepository.save(advertisement);
     }
@@ -32,6 +36,24 @@ public class AdvertisementService {
 
     public Advertisement getAdvertisementById(Long id){
         return advertisementRepository.findAdvertisementById(id);
+    }
+
+    private Calendar getMidnightEndDate(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.SECOND, 59);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        return calendar;
+    }
+
+    private Calendar getMidnightStartDate(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        return calendar;
     }
 
 }
