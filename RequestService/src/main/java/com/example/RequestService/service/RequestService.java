e72520893f83d11618d427e25446979ead5d493d
N3nadM <nenad.marin96@gmail.com>
2020-06-22 00:24:12 +0200

    paying request

--- RequestService/src/main/java/com/example/RequestService/service/RequestService.java
+++ RequestService/src/main/java/com/example/RequestService/service/RequestService.java
@@ -290,16 +290,16 @@ public class RequestService {
 
 
     public Request payRequest(Long requestId, String userEmail) throws CustomException{
-        Request request = requestRepository.getOne(requestId);
+        Request request = requestRepository.findById(requestId).get();
         if(request == null || !request.getUserSentRequest().equals(userEmail))
             throw new CustomException("Unauthorized", HttpStatus.UNAUTHORIZED);
+        request.setPaidState(PaidState.PAID);
+        requestRepository.save(request);
+
         List<Request> requests = requestRepository.findAllPendingByAdIdExceptOne(request.getAdId(), requestId);
         for(Request r : requests) {
             r.setPaidState(PaidState.CANCELED);
         }
-
-        request.setPaidState(PaidState.PAID);
-        requests.add(request);
 
         requestRepository.saveAll(requests);
 
